736dc928f2ab75ae12605dae61839e089b53fdf1
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-16 01:28:36 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
 def square_array(array)
   # your code here
-    array.each do |element|
-      element = element*element
-      array.push(element)
-    end
-    return array 
+    array.each do |element| {element = element*element}  
+    return array
 end
 
 
